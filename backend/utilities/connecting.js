4a30222378823d093ff0cbee8c8522c0a1fcb0d0
A7medAref <ahmedmostafa992001@gmail.com>
2024-08-24 20:22:20 +0300

    dockerizing the app

--- backend/utilities/connecting.js
+++ backend/utilities/connecting.js
@@ -1,15 +1,22 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const AppError = require("./Error");
-
+const productsData = require("../data/products");
+const ProductModel = require("../Model/ProductModel");
 dotenv.config();
 
-const string = process.env.connectionString
-  .replace("myFirstDatabase", process.env.DatabaseName)
-  .replace("<password>", process.env.DatabasePassword);
-mongoose.connect(string, (e) => {
-  if (mongoose.connection.readyState) console.log("connecting to the database");
-  else {
+const string = process.env.connectionString;
+mongoose.connect(string, async (e) => {
+  if (mongoose.connection.readyState) {
+    console.log("connecting to the database");
+    // Check if there is any products in the database
+    if ((await ProductModel.countDocuments()) === 0) {
+      console.log("No products in the database");
+      console.log("Adding products to the database");
+      await ProductModel.insertMany(productsData);
+      console.log("Products added successfully");
+    }
+  } else {
     console.log(
       "Problems in connecting to the database ...shutting down the app"
     );
